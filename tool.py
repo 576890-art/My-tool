f9394a7288d7269de35b8be7e8abdc32ae4b887a
576890-art <arafat342422@gmail.com>
2026-03-12 10:48:14 +0600

    frist commit

---
+++ tool.py
@@ -0,0 +1,20 @@
+import os
+import random
+import string
+
+os.system("clear")
+
+print("==== MyTool ====")
+print("Developer: Arafat")
+print("GitHub: https://github.com/576890-art")
+print("Facebook: https://www.facebook.com/arafat576890")
+print("WhatsApp: 01989333156")
+
+print("\nMenu:")
+print("1. Show Files")
+print("2. Show IP")
+print("3. System Info")
+print("4. Password Generator")
+print("5. Username Generator")
+print("6. Network Info")
+print("7. Exit")
